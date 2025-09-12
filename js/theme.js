9954a813216224ecf427564ebffe887145786416
miladrs75 <miladrs7575@gmail.com>
2025-09-12 17:04:31 +0330

    updated theme button innerHTML

--- js/theme.js
+++ js/theme.js
@@ -1,14 +1,23 @@
 const root = document.documentElement;
 const themeBtns = document.querySelectorAll(".site-header__btn--theme");
+const darkIcon = `<span class="material-symbols-outlined"> dark_mode </span>`;
+const lightkIcon = `<span class="material-symbols-outlined"> wb_sunny </span>`;
 
 themeBtns.forEach((themeBtn) => {
   themeBtn.addEventListener("click", (e) => {
     e.preventDefault();
     if (root.getAttribute("data-theme") === "dark") {
-      root.removeAttribute("data-theme"); 
+      root.removeAttribute("data-theme");
     } else {
-      root.setAttribute("data-theme", "dark"); 
+      root.setAttribute("data-theme", "dark");
     }
+
+    themeBtns.forEach((btn) => {
+      if (root.getAttribute("data-theme") === "dark") {
+        btn.innerHTML = lightkIcon;
+      } else {
+        btn.innerHTML = darkIcon;
+      }
+    });
   });
 });
-
